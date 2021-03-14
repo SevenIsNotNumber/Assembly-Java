384bf6bec553248a6370d64b530471b011c3aa8a
SevenIsNotNumber <69080322+SevenIsNotNumber@users.noreply.github.com>
2021-03-14 16:11:33 +0800

    Update NativePart.sh

--- NativePart.sh
+++ NativePart.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install gcc-9-arm-linux-gnueabi
 arm-linux-gnueabi-as Hello_World.s -o asm.o 
 arm-linux-gnueabi-gcc-9 asm.o -shared -o libasm.so 
+
 # clean-up
 rm asm.o
 rm Hello_World.s
