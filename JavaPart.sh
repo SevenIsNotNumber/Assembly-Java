b9f17875da1c8766b039fc1141b6657782474a88
SevenIsNotNumber <69080322+SevenIsNotNumber@users.noreply.github.com>
2021-03-14 16:33:28 +0800

    Update JavaPart.sh

--- JavaPart.sh
+++ JavaPart.sh
@@ -3,3 +3,6 @@ javac code/bruh/callit.java
 zip -r helloworld.jar $(pwd)
 # java helloworld.jar        # it should print hello world if work
 # oh wait. It wont work coz itz arm
+
+sudo mkdir /Hello
+sudo mv $(pwd) /Hello
