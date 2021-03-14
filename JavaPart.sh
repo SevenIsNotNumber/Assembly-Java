7ea495062a97eff3c1f32ff0cb8a62cb3fe9295a
SevenIsNotNumber <69080322+SevenIsNotNumber@users.noreply.github.com>
2021-03-14 16:01:24 +0800

    Update JavaPart.sh

--- JavaPart.sh
+++ JavaPart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 javac code/bruh/callit.java
 zip -r helloworld.jar $(pwd)
-java helloworld.jar        //it should print hello world if work
+java helloworld.jar        # it should print hello world if work
