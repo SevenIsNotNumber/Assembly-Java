c03f2bd706c97db3b2979c64b5e391ddbf543709
SevenIsNotNumber <69080322+SevenIsNotNumber@users.noreply.github.com>
2021-03-14 16:23:08 +0800

    Update JavaPart.sh

--- JavaPart.sh
+++ JavaPart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 javac code/bruh/callit.java
 zip -r helloworld.jar $(pwd)
-java helloworld.jar        # it should print hello world if work
+# java helloworld.jar        # it should print hello world if work
+# oh wait. It wont work coz itz arm
